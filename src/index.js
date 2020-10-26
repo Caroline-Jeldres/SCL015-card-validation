e742250fbd374ee9070ea1a102943a096cf59853
Caroline-Jeldres <cjeldresm@gmail.com>
2020-10-26 19:40:58 -0300

    se suben cambios del validador de maskify y teoria de luhn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,101 @@
 import validator from './validator.js';
+let nombre;
+let numero_tarjeta;
 
+
+document.getElementById("boton_inicio").onclick = function () {
+    document.getElementById("primera_pagina").style.display = "none";
+    document.getElementById("segunda_pagina").style.display = "block";
+};
+
+
+// document.getElementsByClassName("boton_inicio").addEventListener("click","pasar_pagina_2()",true)
+//     document.getElementById("primera_pagina").style.display = "none";
+//     document.getElementById("segunda_pagina").style.display = "block";
+
+
+
+
+document.getElementById("boton_enviar").onclick = function () {
+    nombre = document.getElementById("nombre").value
+    numero_tarjeta = document.getElementById("numero_tarjeta").value
+    let fecha = document.getElementById("fecha_expiracion").value
+    let cvv = document.getElementById("cvv").value
+
+    if (nombre != "" && numero_tarjeta != "" && fecha != "" && cvv != "") {
+        document.getElementById("segunda_pagina").style.display = "none";
+        document.getElementById("tercera_pagina").style.display = "block";
+
+        document.getElementById("resultados_datos").innerHTML = nombre.toUpperCase()+ "<br>la tarjeta ingresada número " + validator.maskify(numero_tarjeta) + "<br>es "+validator.isValid(numero_tarjeta)
+
+    }
+    else {
+        alert("Faltan ingresar datos")
+    }
+
+}
+// al presionar el boton volver limpia la información almacenada y nos devuelve a la pagina 2 de ingreso de datos 
+document.getElementById("volver").onclick = function () {
+    document.getElementById("tercera_pagina").style.display = "none";
+    document.getElementById("segunda_pagina").style.display = "block";
+    document.getElementById("nombre").value="";
+    document.getElementById("numero_tarjeta").value="";
+    document.getElementById("fecha_expiracion").value="";
+    document.getElementById("cvv").value="";
+    
+}
+// al presionar salir nos recarga la pagina y nos devuelve  la pagina 1 (principal )
+document.getElementById("salir").onclick = function () {
+    document.getElementById("tercera_pagina").style.display = "none";
+    document.getElementById("primera_pagina").style.display = "block";
+    window.location.reload();
+}
+document.addEventListener.onfocusout = () => {
+    let fecha = document.getElementById("fecha_expiracion").value;
+    let mes = parseInt(fecha.split("/")[0]);
+    let ano = parseInt(fecha.split("/")[1]);
+    if (parseInt(mes) > 12) {
+        alert("Fecha incorrecta");
+        document.getElementById("fecha_expiracion").value = null;
+    }
+    if (parseInt(mes) < 1) {
+        alert("Fecha incorrecta");
+        document.getElementById("fecha_expiracion").value = null;
+    }
+    if (parseInt(ano) < 1900) {
+        alert("Fecha incorrecta");
+        document.getElementById("fecha_expiracion").value = null;
+    }
+}
+
+// se ejecuta al escribir
+document.getElementById("fecha_expiracion").addEventListener("keydown", e => {
+    if (e.which != 9 && e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57) && (e.which < 96 || e.which > 105)) {
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+    }
+
+    else {
+        let valor = document.getElementById("fecha_expiracion").value
+        if (valor.length == 2 && e.which != 8) {
+            document.getElementById("fecha_expiracion").value = valor + "/";
+        }
+        
+    }
+})
+document.getElementById("numero_tarjeta").addEventListener("keydown", e => {
+    if (e.which != 9 && e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57) && (e.which < 96 || e.which > 105)) {
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+    }
+    // else {
+    //     let valor = document.getElementById("numero_tarjeta").value
+
+    // }
+})
+
+
+// eslint-disable-next-line no-console
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,53 @@
 const validator = {
   // ...
-};
+  maskify: (tarjeta) => {
+    let numeral = "°";
+    let tarjetaEnmascarada = tarjeta.slice(-4)
+    if (tarjeta.length > 4) {
+      tarjetaEnmascarada = numeral.repeat(tarjeta.length - 4) + tarjeta.slice(-4)
+    }
+    return tarjetaEnmascarada;
+  },
+  isValid: (validacion) => {
+
+    let retorno = false;
+    //se comienza a numerar el largo de la tarjeta desde cero
+    let suma = 0;
+    //Se crea un for que recorre el largo de la numero de la tarjeta obteniendo un numero en cada posicion//
+    for (let i = 0; i < validacion.length; i++) {
+      //se obtiene un nuemro en la posicion i de la cadena del numero de tarjeta
+      let numero = parseInt(validacion.charAt(i));
+      //determina si la variable i contiene un numero par
+      if (i % 2 == 0) {
+        //Se multiplican los pares por dos//
+        let multiplica = numero * 2;
+        if (multiplica > 9) { //los resultados mayores a 9, se la aplica un toString q los separa y los suma uno con el otro
+          let multiplicaString = multiplica.toString();
+          multiplica = parseInt(multiplicaString.charAt(0)) + parseInt(multiplicaString.charAt(1));
+
+        }
+        suma = suma + multiplica;
+
+      } else {
+        suma = suma + numero;
+      }
+    }
+    //determina si la suma es multiplo de 10 (termina en cero)
+    if (suma % 10 == 0) {
+      retorno = true;
+    }
+    if (retorno===true){
+      return "<strong>VALIDA"
+    }
+    else {
+      return "<strong>INVALIDA"
+    }
+    
+    }
+
+  }
+
+
+
 
 export default validator;
