c36ad04ff9a2e00cd28631d0610b0e0544867bd2
Caroline-Jeldres <cjeldresm@gmail.com>
2020-10-27 22:09:01 -0300

    se suben cambios a test

--- test/validator.spec.js
+++ test/validator.spec.js
@@ -10,17 +10,18 @@ describe('validator', () => {
     it('debería ser una función', () => {
       expect(typeof validator.isValid).toBe('function');
     });
-
-    it.skip('debería retornar true para "4083952015263"', () => {
-      expect(validator.isValid(4083952015263)).toBe(true);
+    test('debería retornar true para "4137894711755904"', () => {
+      validator.isValid(validator.isValid('true'), "4137894711755904");
+    // it.skip('debería retornar true para "4083952015263"', () => {
+    //   expect(validator.isValid("4083952015263")).toBe(true);
     });
 
-    it.skip('debería retornar true para "79927398713"', () => {
-      expect(validator.isValid(79927398713)).toBe(true);
+    test('debería retornar true para "79927398713"', () => {
+      validator.isValid(validator.isValid('true'), "79927398713");
     });
 
-    it.skip('debería retornar false para "1234567890"', () => {
-      expect(validator.isValid(1234567890)).toBe(false);
+    test('debería retornar false para "1234567890"', () => {
+      validator.isValid(validator.isValid('false'), "1234567890");
     });
   });
 
@@ -28,17 +29,13 @@ describe('validator', () => {
     it('debería ser una función', () => {
       expect(typeof validator.maskify).toBe('function');
     });
-
-    it.skip('Debería retornar "############5616" para "4556364607935616"', () => {
-      expect(validator.maskify("4556364607935616").toBe(true))
+    test('Debería retornar "############5616" para "4556364607935616"', () => {
+      validator.maskify(validator.maskify('############5616'), "4556364607935616");
     });
-
-    it.skip('Debería retornar "1" para "1"', () => {
-      expect(validator.maskify("1").toBe(false))
+    test('Debería retornar "1" para "1"', () => {
+      validator.maskify(validator.maskify('1'), "1");
     });
-
-    it.skip('Debería retornar "######orld" para "helloworld"', () => {
-      expect(validator.maskify("helloworld").toBe(true))
-    });
+    test('Debería retornar "######orld" para "helloworld"', () => {
+      validator.maskify(validator.maskify("######orld"), "helloworld");
   });
-});
+})})
